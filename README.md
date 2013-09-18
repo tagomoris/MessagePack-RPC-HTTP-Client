1dc7122b94d45d7ca63b85c6882a6e1c50117ae2
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-09-18 21:58:51 +0900

    init with minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MessagePack::RPC::HTTP::Client - It's new $module
+
+# SYNOPSIS
+
+    use MessagePack::RPC::HTTP::Client;
+
+# DESCRIPTION
+
+MessagePack::RPC::HTTP::Client is ...
+
+# LICENSE
+
+Copyright (C) TAGOMORI Satoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+TAGOMORI Satoshi <tagomoris@gmail.com>
---
+++ lib/MessagePack/RPC/HTTP/Client.pm
@@ -0,0 +1,39 @@
+package MessagePack::RPC::HTTP::Client;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MessagePack::RPC::HTTP::Client - It's new $module
+
+=head1 SYNOPSIS
+
+    use MessagePack::RPC::HTTP::Client;
+
+=head1 DESCRIPTION
+
+MessagePack::RPC::HTTP::Client is ...
+
+=head1 LICENSE
+
+Copyright (C) TAGOMORI Satoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+TAGOMORI Satoshi E<lt>tagomoris@gmail.comE<gt>
+
+=cut
+
